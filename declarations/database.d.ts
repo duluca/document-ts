ce07bb44b65b4368b8aba8f8eef72b0534e3aa90
Doguhan Uluca <duluca@gmail.com>
2017-02-04 02:35:22 -0500

    cleanup declerations

--- declarations/database.d.ts
+++
@@ -1,3 +0,0 @@
-import { Db } from 'mongodb';
-export declare function connect(mongoUri: string, isProduction?: boolean, certFileUri?: string): Promise<void>;
-export declare function getDbInstance(): Db;
--- declarations/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './database';
-export * from './collectionFactory';
-export * from './document';
