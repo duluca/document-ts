af01b1ddb4108e6c2046b859f4041259b40d9729
Doguhan Uluca <duluca@gmail.com>
2019-10-30 01:14:27 -0400

    ci updates

--- dist/database.d.ts
+++ dist/database.d.ts
@@ -1,5 +1,5 @@
 import { Db } from 'mongodb';
-export declare function connect(mongoUri: string, isProduction?: boolean, connectionRetryWait?: number, connectionRetryMax?: number, certFileUri?: string): Promise<void>;
-export declare function close(force?: boolean): Promise<void>;
-export declare function connectionStatus(): boolean;
+export declare function connect(mongoUri: string, isProduction?: boolean, connectionRetryWait?: number, connectionRetryMax?: number, certFileUri?: string): any;
+export declare function close(force?: boolean): any;
+export declare function connectionStatus(): any;
 export declare function getDbInstance(): Db;
