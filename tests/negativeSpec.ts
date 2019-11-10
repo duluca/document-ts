a2c53bd9b8489f3243782ae735574448476a5b72
Kenton Bocock <kentonbo@gmail.com>
2019-11-10 08:59:14 -0500

    add negative spec for error testing.

---
+++ tests/negativeSpec.ts
@@ -0,0 +1,42 @@
+'use strict'
+
+import { ObjectID } from 'mongodb'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+import { close, connect } from '../lib/index'
+import { IUser, User, UserCollection } from './user'
+
+let mongoServerInstance: MongoMemoryServer
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+
+describe('Document', () => {
+  beforeEach(async () => {
+    mongoServerInstance = new MongoMemoryServer({ instance: { dbName: 'testDb' } })
+    const uri = await mongoServerInstance.getConnectionString()
+    await connect(uri)
+  })
+
+  afterEach(async () => {
+    await close()
+    await mongoServerInstance.stop()
+  })
+
+  it('should get an error when query is null with pagination', async () => {
+    const expectedException = null
+    let actualException = null
+    const expectedRecordCount = 20
+
+    try {
+      for (let i = 0; i < expectedRecordCount; i++) {
+        const user = new User()
+        await user.create(`${i}`, `${i}`, `${i}@gmail.com`, 'user')
+      }
+    } catch (ex) {
+      actualException = ex
+    }
+
+    expect(expectedException).toEqual(actualException)
+
+    await expectAsync(UserCollection.findWithPagination<User>(undefined)).toBeRejected()
+  })
+})
